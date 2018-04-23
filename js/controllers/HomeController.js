a43a5a55e05392b7a0f9f21a959c38cc41b67e0d
skyriser92 <35499411+skyriser92@users.noreply.github.com>
2018-04-23 14:28:30 -0400

    followed instructions up to step 5
    added angular link, corrected all original variables to function

--- js/controllers/HomeController.js
+++ js/controllers/HomeController.js
@@ -98,8 +98,9 @@ app.controller('HomeController', ['$scope', function($scope) {
 	
 	/* ADD VARIABLES FOR STEP 3 HERE */
 	
-	
-	
+	$scope.title = "IMDB + John's Top 8 Movies";
+	$scope.owner = "John McMinn";
+	$scope.github = " http://www.github.com/skyriser/is219s18mcminn-p3";
 	
 	
 	
